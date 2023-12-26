4b38539c7fffc66d7bfbcf4c289696d3b779fe15
Tengku rafi <t.mrr39@gmail.com>
2023-12-26 11:02:03 +0700

    feat: resolver function

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema.ts';
+import { typeDefs } from './schema.js';
+import db from './_db.js';
 
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.authors
+        },
+        authors() {
+            return db.reviews
+        }
+    }
+}
 const server = new ApolloServer({
     typeDefs,
-    // resolvers
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
